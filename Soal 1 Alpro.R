6f9b9ac0a1f8a38ce5231f08be12ba4e336a5773
maryantiayudwi-creator <maryantiayudwi@gmail.com>
2026-06-09 13:59:32 +0700

    Add files via upload

---
+++ Soal 1 Alpro.R
@@ -0,0 +1,10 @@
+teks <- readline("Masukkan teks: ")
+
+kata <- strsplit(teks, " ")[[1]]
+jumlah_kata <- length(kata)
+
+kalimat <- strsplit(teks, "\\.")[[1]]
+jumlah_kalimat <- length(kalimat) - 1
+
+cat("Jumlah kata =", jumlah_kata, "\n")
+cat("Jumlah kalimat =", jumlah_kalimat, "\n")
