e572202634d2302688fc2f652c9c9a4a3fb5c8d3
Maheswari1510 <bebas4kun0@gmail.com>
2026-06-16 12:39:18 +0700

    Update 02_string_kalimat.R

--- src_R/02_string_kalimat.R
+++ src_R/02_string_kalimat.R
@@ -1,12 +1,14 @@
-K1 <- "Saya tak 'kan menyerah."
+tampilkan_kalimat <- function() {
+  K1 <- "Saya tak 'kan menyerah."
+  K2 <- 'Ia berkata, "Aku menyayangimu."'
+  K3 <- "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+  K4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023."
+  
+  cat("K1:", K1, "\n")
+  cat("K2:", K2, "\n")
+  cat("K3:", K3, "\n")
+  cat("K4:", K4, "\n")
+}
 
-K2 <- 'Ia berkata, "Aku menyayangimu."'
-
-K3 <- "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
-
-K4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023."
-
-cat(K1, "\n")
-cat(K2, "\n")
-cat(K3, "\n")
-cat(K4, "\n")
+#contoh
+tampilkan_kalimat()
